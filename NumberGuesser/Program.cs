655499f304d9d5924512e5b256125f45d135893f
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-18 06:44:18 +0100

    zacatek

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+// Namespace
 namespace NumberGuesser
 {
+    // Main Class
     internal class Program
     {
+        //Entry point method
         static void Main(string[] args)
         {
+            //Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Pavel";
+
+            //Change text color 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            //Reset text color
+            Console.ResetColor();
         }
     }
 }
